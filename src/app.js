24094ad8da6678909b4adb19dd5177d19b2cbaed
MacServer <942385056@qq.com>
2022-03-26 17:52:16 +0800

    收货地址接口开发

--- src/app.js
+++ src/app.js
@@ -1,63 +1,74 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const session = require('koa-generic-session')
-const cors = require('koa-cors')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const session = require("koa-generic-session");
+const cors = require("koa-cors");
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require("./routes/index");
+const users = require("./routes/users");
+const addressRouter = require("./routes/addressRouter");
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug",
+  })
+);
 
 //设置cors跨域请求
-app.use(cors({
-  origin:'http://localhost:8080',//前端origin
-  credentials:true//允许跨域带cookie
-}))
+app.use(
+  cors({
+    origin: "http://localhost:8080", //前端origin
+    credentials: true, //允许跨域带cookie
+  })
+);
 
 //设置秘钥
-app.keys=['solomonKey']
+app.keys = ["solomonKey"];
 
 //设置session
-app.use(session({
-  cookie:{//设置cookie
-    path:'/',
-    httpOnly:true,
-    maxAge:24*60*60*1000
-  }
-}))
+app.use(
+  session({
+    cookie: {
+      //设置cookie
+      path: "/",
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
-module.exports = app
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+app.use(addressRouter.routes(), addressRouter.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
